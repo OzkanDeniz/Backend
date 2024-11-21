49f820949c13ac05fbd4e08067db9e0ae8145745
ozkan <ozkandnz09@gmail.com>
2024-11-22 00:14:54 +0300

    app.get

--- Express-Routing/app.js
+++ Express-Routing/app.js
@@ -13,5 +13,14 @@ const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 const HOST = process.env.HOST || "127.0.0.1";
+/*------------------------*/
 
+app.get('/', (req, res)=>{
+console.log('get atıldı')
+
+    res.send('GEt yapıldı')
+})
+
+
+/*------------------------*/
 app.listen(PORT, () => {console.log(`Running: http://${HOST}:${PORT}`)});
