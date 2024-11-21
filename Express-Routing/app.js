43752af40e04d433c2ba051dd668df9b51a6f7a8
ozkan <ozkandnz09@gmail.com>
2024-11-21 23:34:15 +0300

    npm init -y npm i express dotenv

--- Express-Routing/app.js
+++ Express-Routing/app.js
@@ -1,10 +1,13 @@
 "use strict"
 /*--------------------------
     EXPRESSJS
---------------------------*/
+--------------------------*
+* nmp init - y
+* nmp i express dotenv
 
 /*------------------------*/
-
+const express = require("express")
+const app = express
 
 
 
