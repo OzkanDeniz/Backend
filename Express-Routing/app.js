00384b6428f891325b0f26852bf339adecc28fec
ozkan <ozkandnz09@gmail.com>
2024-11-22 12:49:22 +0300

    method all

--- Express-Routing/app.js
+++ Express-Routing/app.js
@@ -16,36 +16,54 @@ const HOST = process.env.HOST || "127.0.0.1";
 /*------------------------*/
 
 //HTTP METHOD(GET,POST,PUT,DELETE) & URLs('/')
-
-app.get('/', (req, res)=>{
-console.log('get atıldı')
+//!(1)
+// app.get('/', (req, res)=>{
+// console.log('get atıldı')
  
-    res.send({
-        message:'welcome to express'
-    })
-})
+//     res.send({
+//         message:'welcome to express'
+//     })
+// })
 
-app.post('/', (req, res)=>{
-    console.log('get atıldı')
+// app.post('/', (req, res)=>{
+//     console.log('get atıldı')
      
-        res.send({
-            message:'run post'
-        })
-    })
-app.put('/', (req, res)=>{
-    console.log('get atıldı')
+//         res.send({
+//             message:'run post'
+//         })
+//     })
+// app.put('/', (req, res)=>{
+//     console.log('get atıldı')
      
-        res.send({
-            message:'run put'
-        })
-    })
-app.delete('/', (req, res)=>{
-    console.log('get atıldı')
+//         res.send({
+//             message:'run put'
+//         })
+//     })
+// app.delete('/', (req, res)=>{
+//     console.log('get atıldı')
      
-        res.send({
-            message:'run delete'
-        })
-    })
+//         res.send({
+//             message:'run delete'
+//         })
+//     })
+
+//!(2)
+//METHOD ALL (ALL=GET,POST,PUT,DELETE)
+
+// app.all('/example', (req,res)=>{
+//     res.send({
+//         message:'run all'
+//     })
+// })
+
+//!(3)
+
+app.route('/route')
+.get((req,res)=>{res.send({message:'get'})})
+.post((req,res)=>{res.send({message:'post'})})
+.put((req,res)=>{res.send({message:'put'})})
+.delete((req,res)=>{res.send({message:'delete'})})
+
 
 
 /*------------------------*/
