2d6ffad8f4ed59ac59fb80e1e6d49c9188e23abf
ozkan <ozkandnz09@gmail.com>
2024-11-22 13:55:51 +0300

    url path options

--- Express-Routing/app.js
+++ Express-Routing/app.js
@@ -15,8 +15,9 @@ const PORT = process.env.PORT || 8000;
 const HOST = process.env.HOST || "127.0.0.1";
 /*------------------------*/
 
-//HTTP METHOD(GET,POST,PUT,DELETE) & URLs('/')
+
 //!(1)
+//?HTTP METHOD(GET,POST,PUT,DELETE) & URLs('/')
 // app.get('/', (req, res)=>{
 // console.log('get atıldı')
  
@@ -48,7 +49,7 @@ const HOST = process.env.HOST || "127.0.0.1";
 //     })
 
 //!(2)
-//METHOD ALL (ALL=GET,POST,PUT,DELETE)
+//?METHOD ALL (ALL=GET,POST,PUT,DELETE)
 
 // app.all('/example', (req,res)=>{
 //     res.send({
@@ -57,14 +58,34 @@ const HOST = process.env.HOST || "127.0.0.1";
 // })
 
 //!(3)
-//METHOD (APP.ROUTE)
-app.route('/route')
-.get((req,res)=>{res.send({message:'get'})})
-.post((req,res)=>{res.send({message:'post'})})
-.put((req,res)=>{res.send({message:'put'})})
-.delete((req,res)=>{res.send({message:'delete'})})
-
-
+//?METHOD (APP.ROUTE)
+// app.route('/route')
+// .get((req,res)=>{res.send({message:'get'})})
+// .post((req,res)=>{res.send({message:'post'})})
+// .put((req,res)=>{res.send({message:'put'})})
+// .delete((req,res)=>{res.send({message:'delete'})})
 
 /*------------------------*/
+
+//?URL (path) options
+// app.get('/',(req,res)=>res.send('/ = root (home)'))
+// app.get('/path',(req,res)=>res.send('/path = path'))
+
+//?express-url supported JokerChars
+// app.get('/abc(x)?123',(req,res)=>res.send('/abc(x)?123))
+// app.get('/abc(x)+123',(req,res)=>res.send('/abc(x)+123))
+// app.get('/abc*123',(req,res)=>res.send('/abc*123))
+// app.get('/abc*123',(req,res)=>res.send('/abc*123))
+
+//?express-url supported RegExr
+// app.get(/xyz/,(req,res)=>res.send('/xyz/'))  /içerisinde yazılmalı/ 
+// app.get(/xyz$/,(req,res)=>res.send('/xyz/'))    xyz ile biten
+// app.get(/^\/xyz/,(req,res)=>res.send('/xyz/'))    /xyz ile başlayan    (\ işaretinden sonra gelen işareti özel karakter olarak algılama kaçış karakteri demek)
+
+
+
+
+
+
+
 app.listen(PORT, () => {console.log(`Running: http://${HOST}:${PORT}`)});
