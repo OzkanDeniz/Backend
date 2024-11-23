b362a2064d0287c9cb164135a656ceb47d3b62d1
ozkan <ozkandnz09@gmail.com>
2024-11-24 00:05:44 +0300

    router end

--- Routers-Middlewares/router.js
+++ Routers-Middlewares/router.js
@@ -5,8 +5,8 @@ const app = express();
 
 require("dotenv").config();
 const PORT = process.env.PORT || 8000;
-
-//? Router is a special app for URL control in ExpressJS.
+/*-----------------------------------------------*/
+// ? Router is a special app for URL control in ExpressJS.
 
 // app.get('/welcome', function(req,res){
 //     res.send({
@@ -22,9 +22,7 @@ const PORT = process.env.PORT || 8000;
 // router.put("/", (req,res) => {res.send({ message: "put"}) });
 // router.delete("/", (req,res) => {res.send({ message: "delete"}) });
 
-
 // router.all("/", (req,res) => {res.send({ message: "all"}) });
-
 
 // router.route('/')
 //     .get((req,res) => {res.send({ message: "welcome"}) })
@@ -32,10 +30,15 @@ const PORT = process.env.PORT || 8000;
 //     .put((req,res) => {res.send({ message: "put"}) })
 //     .delete((req,res) => {res.send({ message: "delete"}) });
 
-
-//app e router kullandığımı bildiriyoruz
+// app e router kullandığımı bildiriyoruz
 // app.use(router)
 
+//? Router ayarlarını /router/index.js dosyasından al
 
+// const router = require('./routes/index.js')
+// const router = require('./routes/index')
+const router = require("./routes/"); //default dosya ismi her zaman index.js tir.
+
+app.use(router);
 
 app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
