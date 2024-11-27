30d8fd1c49a5fe3788602436d9ba8b6a367d3c20
ozkan <ozkandnz09@gmail.com>
2024-11-27 12:47:15 +0300

    middleware without app.use

--- Routers-Middlewares/middleware.js
+++ Routers-Middlewares/middleware.js
@@ -29,27 +29,91 @@ const PORT = process.env.PORT;
 //   });
 // });
 
-app.get("/", (req, res, next) => {
-  if (req.query.username == "clarusway") {
-    //?req ve res ile data taşıma:
-    //next() ile req ve res bir sonraki route a aktarılır
-    req.username = "clarusway";
-    res.message = 'username is clarusway'
-    next();
+// app.get("/", (req, res, next) => {
+//   if (req.query.username == "clarusway") {
+//     //?req ve res ile data taşıma:
+//     //next() ile req ve res bir sonraki route a aktarılır
+//     req.username = "clarusway";
+//     res.message = 'username is clarusway'
+//     next();
 
-  } else {
-    res.send({
-      message: " Error: username is wrong",
-    });
-  }
-});
+//   } else {
+//     res.send({
+//       message: " Error: username is wrong",
+//     });
+//   }
+// });
 
-app.get("/", (req, res) => {
+// app.get("/", (req, res) => {
+//   res.send({
+//     username: req.username,
+//     // message: "username is true",
+//     message: res.message
+//   });
+// });
+
+//?Data Taşıma
+// app.get("/", (req, res, next) => {
+//   req.message1 = "middleware1";
+//   next();
+// });
+// app.get("/", (req, res, next) => {
+//   req.message2 = "middleware2";
+//   next();
+// });
+// app.get("/", (req, res, next) => {
+//   req.message3 = "middleware3";
+//   next();
+// });
+// app.get("/", (req, res, next) => {
+//   req.message4 = "middleware4";
+//   next();
+// });
+// app.get("/", (req, res, next) => {
+//   req.message5 = "middleware5";
+//   next();
+// });
+
+// app.get("/", (req, res) => {
+//   res.send({
+//     message1: req.message1,
+//     message2: req.message2,
+//     message3: req.message3,
+//     message4: req.message4,
+//     message5: req.message5,
+//     message: "Finished",
+//   });
+// });
+
+
+//?Middleware Func
+const middleFunc1 = (req, res, next) => {
+  console.log('middleFunc1 is run')
+  req.message1 = 'middleFunc1 run'
+  next()
+};
+
+const middleFunc2 = (req, res, next) => {
+  console.log('middleFunc2 is run')
+  req.message2 = 'middleFunc1 run'
+  next()
+};
+
+//? app.use(middleFunc1,middleFunc2)
+//! app.use([middleFunc1,middleFunc2]) All methods (recommended)
+// app.get('/home',[middleFunc1,middleFunc2]) get method (url must be)
+
+//! Ayrıca app.use kullanmadan path routerda middleware gönderebiliriz
+
+app.get('/home',[middleFunc1,middleFunc2],(req,res)=>{
   res.send({
-    username: req.username,
-    // message: "username is true",
-    message: res.message 
-  });
-});
+    message1: req.message1,
+    message2: req.message2,
+    message:'Finished'
+  })
+
+})
+
+
 
 app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
