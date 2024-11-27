5b35b0814ba2ee7b9f4e57703a666adf9349fa7c
ozkan <ozkandnz09@gmail.com>
2024-11-27 13:19:38 +0300

    module export (Defining a function within an object)

--- Routers-Middlewares/middlewares/index.js
+++ Routers-Middlewares/middlewares/index.js
@@ -1,16 +1,47 @@
 "use strict";
 
-const middleFunc1 = (req, res, next) => {
-  console.log("middleFunc1 is run");
-  req.message1 = "middleFunc1 run";
-  next();
-};
+//? 1.yol
+// const middleFunc1 = (req, res, next) => {
+//   console.log("middleFunc1 is run");
+//   req.message1 = "middleFunc1 run";
+//   next();
+// };
 
-const middleFunc2 = (req, res, next) => {
-  console.log("middleFunc2 is run");
-  req.message2 = "middleFunc1 run";
-  next();
-};
+// const middleFunc2 = (req, res, next) => {
+//   console.log("middleFunc2 is run");
+//   req.message2 = "middleFunc1 run";
+//   next();
+// };
 
-// module.exports = [middleFunc1,middleFunc2]
-module.exports = {middleFunc1,middleFunc2}
+//  module.exports = [middleFunc1,middleFunc2]
+// module.exports = {middleFunc1,middleFunc2}
+
+//? 2.yol
+
+// module.exports.middleFunc1 = (req, res, next) => {
+//   console.log("middleFunc1 is run");
+//   req.message1 = "middleFunc1 run";
+//   next();
+// };
+
+// module.exports.middleFunc2 = (req, res, next) => {
+//   console.log("middleFunc1 is run");
+//   req.message1 = "middleFunc1 run";
+//   next();
+// };
+
+//? 3.yol
+
+module.exports = {
+  middleFunc1: (req, res, next) => {
+    console.log("middleFunc1 is run");
+    req.message1 = "middleFunc1 run";
+    next();
+  },
+  
+  middleFunc2: (req, res, next) => {
+    console.log("middleFunc2 is run");
+    req.message2 = "middleFunc2 run";
+    next();
+  },
+};
